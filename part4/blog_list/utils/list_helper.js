fc90d3cac0a509b02bd1385d164eb20fa2935d6f
Timo Vehvilainen <timo.t.vehvilainen@gmail.com>
2019-11-08 16:36:35 +0200

    part 4.5 stable

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -9,7 +9,19 @@ const totalLikes = (blogs) => {
   }, 0))
 }
 
+const favouriteBlog = (blogs) => {
+  return (
+    blogs.reduce((mostLikedBlog, blog) => {
+      if (blog.likes > mostLikedBlog.likes) {
+          return ((({ title, author, likes}) => ({ title, author, likes }))(blog))
+      } 
+      return (mostLikedBlog)
+  }, {likes:-1})
+  )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
