44c6417efeee1c3db77da5f1debcc2aa206febf7
Timo Vehvilainen <timo.t.vehvilainen@gmail.com>
2019-11-10 23:19:31 +0200

    part 4.20 done

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -11,18 +12,27 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  const blog = new Blog(
-    {
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      user: user._id
-    }
-  )
+  const token = request.token
+  
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ 
+        error: 'token missing or invalid' 
+      })
+    }
+
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog(
+      {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+      }
+    )
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
